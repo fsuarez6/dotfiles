f4d900cee59c5ec251c44e520950a6bb789bb7c3
fsuarez6 <fsuarez6@gmail.com>
2015-12-06 17:06:38 +0800

    Added default latex tags and terminator configuration

--- update_local_files.sh
+++ update_local_files.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 # Geany configuration
+cp -r geany $HOME/.config
 wget http://download.geany.org/contrib/tags/std.latex.tags
 mv std.latex.tags $HOME/.config/geany/tags
-cp -r geany $HOME/.config
+# Terminator configuration
+cp -r terminator $HOME/.config
 # bashrc
 cp .bash_aliases $HOME
 echo "Done!"
