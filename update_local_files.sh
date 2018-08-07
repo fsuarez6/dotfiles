02c99842ec0ee4387048c9f1a90477ecbbe8010f
fsuarez6 <fsuarez6@gmail.com>
2018-08-07 10:50:14 +0800

    Add atom styles

--- update_local_files.sh
+++ update_local_files.sh
@@ -7,9 +7,10 @@ if [ -f $DST ] || [ -L $DST ]; then
   rm $DST
 fi
 ln -s `realpath $BASEDIR/.bash_aliases` $DST
+echo "Added symlink: $DST"
 
 # Atom
-CONFIG_FILES=$(find $BASEDIR/.atom -type f -name *.cson)
+CONFIG_FILES=$(find $BASEDIR/atom -maxdepth 1 -type f -name *.cson -o -name *.less)
 for FILEPATH in $CONFIG_FILES; do
   FILENAME=$(basename "$FILEPATH")
   DST=$HOME/.atom/$FILENAME
@@ -17,6 +18,7 @@ for FILEPATH in $CONFIG_FILES; do
     rm $DST
   fi
   ln -s `realpath $FILEPATH` $DST
+  echo "Added symlink: $DST"
 done
 
 echo "Done!"
